fb9d2f29bd57941a842fcd174aa8dd2b645a418c
Muhammad Tamim <mdt40234@gmail.com>
2023-08-27 00:11:53 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+import './Header.css'
+import logo from '../img/Logo.svg';
+// /src/Logo.svg
+const Header = () => {
+    return (
+        <div className="nav">
+            <img src={logo} alt="" />
+            <div>
+                <a href="/Shop">Shop</a>
+                <a href="/Order">Order Review</a>
+                <a href="Inventory">Manage Inventory</a>
+                <a href="Login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
