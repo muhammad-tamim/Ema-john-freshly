c676692e2931b50e69c0b02f7d36b174e7e48bd3
Muhammad Tamim <mdt40234@gmail.com>
2023-09-21 23:26:45 +0600

    install fontAwesome, added shopping icon and dynamically selected item added

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Products.css'
 
 const Products = (props) => {
-    const {name,seller,price,img,ratings}=props.products;
-    console.log(props);
+    // console.log(props);
+    const { name, seller, price, img, ratings } = props.products;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className="product-info">
-            <h3>Name : {name}</h3>
-            <h5>Price : ${price}</h5>
-            <p>Manufacture : {seller}</p>
-            <p>Rating : {ratings}</p>
+                <h3>Name : {name}</h3>
+                <h5>Price : ${price}</h5>
+                <p>Manufacture : {seller}</p>
+                <p>Rating : {ratings}</p>
             </div>
-            <button className='add-btn'>Add to Cart</button>
+            <button onClick={() => handleAddToCart(props.products)} className='add-btn'>
+                Add to Cart <FontAwesomeIcon icon={faShoppingCart} />
+
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,28 +3,36 @@ import './Shop.css'
 import Products from '../Products/Products';
 
 const Shop = () => {
-    const [products,setProduct]=useState([]);
+    const [products, setProduct] = useState([]);
+    const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('products.json')
-        .then(res=>res.json())
-        .then(data=>setProduct(data))
-    },[]);
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, []);
+
+    const handleAddToCart = products => {
+        const newCart = [...cart, products];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-containar'>
             <div className="products-containar">
                 {
                     products.map(
-                        products=><Products
-                        key={products.id}
-                        products={products}
+                        products => <Products
+                            key={products.id}
+                            products={products}
+                            handleAddToCart={handleAddToCart}
                         ></Products>
                     )
                 }
             </div>
             <div className="cart-containar">
                 <h2 className='order-title'>Order Summary</h2>
-                <p>Selected Items: 6</p>
+                <p>Selected Items: {cart.length}</p>
                 <p>Total Price: $1140</p>
                 <p>Total Shipping Charge: $5</p>
                 <p>Tax: $114</p>
